d9e0a8bf98760c865519fd2fde7c3188f8cba044
pietroparreira <pietro.parreira@edu.senai.br>
2026-04-28 09:05:01 -0300

    v1.0 conversão de moedas

---
+++ bibliotecaFuncoes.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace conversao {
+    function dolarParareal($valor, $cotacao)
+    {
+        return $valor * $cotacao;
+    }
+    function euroParareal($valor, $cotacao)
+    {
+        return $valor * $cotacao;
+    }
+    function pesoParareal($valor, $cotacao)
+    {
+        return $valor * $cotacao;
+    }
+    function libraParareal($valor, $cotacao)
+    {
+        return $valor * $cotacao;
+    }
+    function ieneParareal($valor, $cotacao)
+    {
+        return $valor * $cotacao;
+    }
+}
