9f3b7f52fa9b42b84c23e7fc05e408c7bd986feb
pietroparreira <pietro.parreira@edu.senai.br>
2026-04-28 09:25:38 -0300

    v1.2 conversão de moedas
    adicionei o arquivo errado na primeira, agora esses são os arquivos certos

--- entradaDados.php
+++ entradaDados.php
@@ -1,23 +1,23 @@
 <?php
 
-require_once "bibliotcaFuncoes";
+require_once "bibliotecaFuncoes.php";
 
 use function conversao\dolarParareal;
 
-echo "Dolar para real: ", dolarParareal(50, 5);
+echo "Dolar para real: ", dolarParareal(50, 5), "\n";
 
 use function conversao\euroParareal;
 
-echo "Euro para real: ", euroParareal(50, 5.85);
+echo "Euro para real: ", euroParareal(50, 5.85), "\n";
 
 use function conversao\pesoParareal;
 
-echo "Peso para real: ", Pesoparareal(50, 0.0035183);
+echo "Peso para real: ", Pesoparareal(50, 0.0035183), "\n";
 
 use function conversao\libraparareal;
 
-echo "Libra para real: ", libraParareal(50, 6.71);
+echo "Libra para real: ", libraParareal(50, 6.71), "\n";
 
 use function conversao\ieneParareal;
 
-echo "Ine para real: ", ieneParareal(50, 0.031);
+echo "Ine para real: ", ieneParareal(50, 0.031), "\n";
